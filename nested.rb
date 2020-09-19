788aee0884887ecaebea4dcd4f0480e30b111804
Liselle Logan <liselle.logan@gmail.com>
2020-09-19 17:18:13 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -63,9 +63,8 @@ programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
 	programmer_hash[:yukihiro_matsumoto] = {
-		:known_for =>, "Ruby",
+		:known_for => "Ruby",
 		:languages => ["LISP", "C"]
 	}
 
